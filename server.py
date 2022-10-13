3b06d9baac7edacb2976c3d57abfbc37e4d31086
Phil415 <phillevy415@gmail.com>
2022-10-12 19:23:27 -0700

    userscr commit

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'rootroot'
+app.config['MYSQL_DB'] = 'user_crud_mod'
+
+mysql = MySQL(app)
+
+@app.route ('/users')
+def users():
+    cur = mysql.connection.cursor()
+    sql = "SELECT * FROM users"
+    cur.execute(sql)
+    result = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    return render_template("users.html", result=result)
+
+@app.route ('/users/new', methods=["GET", "POST"])
+def users_new():
+    if request.form:
+        cur = mysql.connection.cursor()
+        sql = "INSERT INTO users (first_name, last_name, email) VALUES ('" + request.form.get('firstname') + "', '"+ request.form.get('lastname') + "', '"+request.form.get('email') +"')"
+        cur.execute(sql)
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('users'))
+        
+    return render_template("users_new.html")
+
+app.run(debug=True)
+
+
+
